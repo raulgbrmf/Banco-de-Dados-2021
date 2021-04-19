8bfd546d6d412b970e85fdd51f37b525c897b51f
Carlos Hammes <81029906+CaduHammes@users.noreply.github.com>
2021-04-19 20:46:09 -0300

    Update Script_PRODUTO_DESCRICAO.sql

--- Scrip_SELECT/Script_PRODUTO_DESCRICAO.sql
+++ Scrip_SELECT/Script_PRODUTO_DESCRICAO.sql
@@ -1,7 +1,7 @@
+--retorna os dados de descrição e produto relacionando-os e os ordenando em ordem crescente.
+
 SELECT p.id, p.nm_produto , dp.cor, dp.peso, dp.dt_fabricacao, dp.valor
 FROM PRODUTO p, DESCRICAO_PRODUTO dp 
 WHERE dp.id = p.id_descricao
 GROUP BY dp.valor
 ORDER BY COUNT(p.id)ASC ;
-
-
