dee78fab244e4a818174449533fd78249869f259
matheusmorsch <82172593+matheusmorsch@users.noreply.github.com>
2021-04-19 22:58:37 -0300

    Update DELECT.sql

--- Trabalho-Final-BD/DELECT.sql
+++ Trabalho-Final-BD/DELECT.sql
@@ -1,20 +1,13 @@
-SELECT Nome_Produto,Valor_Unitario 
+SELECT ID_Produto Nome_Produto,Valor_Unitario 
 FROM Produto
 WHERE (ID_Produto = 5);
 
+DELETE FROM Produto
+WHERE Valor_Unitario = '600'
 
-DELETE FROM Produto;
-WHERE Valor_Unitario = 600
+INSERT INTO Produto (ID_Vendedor, ID_Categoria, ID_Descricao_Produto, Valor_Unitario, Nome_Produto, Quantidade_Estoque, Data_Fabricacao)
+VALUES ('2','5','5','600.00','Sofá','600','2020-10-19');
 
-
-SELECT Nome_Produto,Valor_Unitario 
-FROM Produto
-WHERE (ID_Produto = 5);
-
-
-INSERT INTO Produto(Valor_Unitario)
-VALUES('600.00');
-
-SELECT Nome_Produto,Valor_Unitario 
-FROM Produto
-WHERE (ID_Produto = 5);
+UPDATE Produto 
+SET ID_Produto = 5
+WHERE (ID_Produto = 6)
